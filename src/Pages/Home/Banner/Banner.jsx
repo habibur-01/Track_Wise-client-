2905ca920af09e9d836d6600ff1ca12b0072357c
habibur-01 <h.r.habibur08@gmail.com>
2024-04-06 05:41:57 +0600

    added banner component

--- src/Pages/Home/Banner/Banner.jsx
+++ src/Pages/Home/Banner/Banner.jsx
@@ -1,3 +1,6 @@
+import banner1 from "../../../assets/high-angle-woman-holding-laptop.jpg"
+import banner2 from "../../../assets/3d-view-map.jpg"
+import banner4 from "../../../assets/pexels-pixabay-38271.jpg"
 import { Swiper, SwiperSlide } from 'swiper/react';
 
 // Import Swiper styles
@@ -9,26 +12,28 @@ import { Pagination, Navigation } from 'swiper/modules';
 
 const Banner = () => {
     return (
-        <div>
-            <Swiper
-                pagination={{
-                    type: 'progressbar',
-                }}
-                navigation={true}
-                modules={[Pagination, Navigation]}
-                className="mySwiper"
-            >
-                <SwiperSlide>Slide 1</SwiperSlide>
-                <SwiperSlide>Slide 2</SwiperSlide>
-                <SwiperSlide>Slide 3</SwiperSlide>
-                <SwiperSlide>Slide 4</SwiperSlide>
-                <SwiperSlide>Slide 5</SwiperSlide>
-                <SwiperSlide>Slide 6</SwiperSlide>
-                <SwiperSlide>Slide 7</SwiperSlide>
-                <SwiperSlide>Slide 8</SwiperSlide>
-                <SwiperSlide>Slide 9</SwiperSlide>
-            </Swiper>
-        
+        <div className="relative">
+            <div className="w-full h-[800px]">
+                <Swiper
+                    pagination={{
+                        type: 'progressbar',
+                    }}
+                    navigation={true}
+                    modules={[Pagination, Navigation]}
+                    className="mySwiper"
+                >
+                    <SwiperSlide><img src={banner1} className="h-[800px] w-full object-fill brightness-50" alt="" /></SwiperSlide>
+                    <SwiperSlide><img src={banner2} alt="" className="h-[800px] w-full object-fill" /></SwiperSlide>
+                    <SwiperSlide><img src={banner4} alt="" className="h-[800px] w-full object-fill" /></SwiperSlide>
+
+                </Swiper>
+            </div>
+            <div className="absolute top-64 z-10 text-6xl text-white space-y-6 left-20">
+                <h1 className="font-bold ">Are you confused? </h1>
+                <p className="text-3xl">Bus is gone or not!! {"Let's"} start <br/>  to track your bus.</p>
+                <button className="px-4 py-3 text-base bg-gradient-to-r rounded-md from-indigo-500 via-purple-500 to-pink-500 uppercase">{"Let's Track"}</button>
+            </div>
+
         </div >
     );
 };
