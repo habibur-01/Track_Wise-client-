114e133e920e3c62685eb37d35a3199c4ba9c163
habibur-01 <h.r.habibur08@gmail.com>
2024-04-19 11:51:48 +0600

    login and registration with firebase

--- src/Pages/Login/Login.jsx
+++ src/Pages/Login/Login.jsx
@@ -1,7 +1,28 @@
 import { FaArrowLeft } from "react-icons/fa";
 import loginbg from "../../assets/4957136.jpg"
+import useAuth from "../../hooks/useAuth"
 import "./login.css"
+import { useNavigate } from "react-router-dom";
 const Login = () => {
+    const {logInUser} = useAuth()
+    const navigate = useNavigate()
+    const handleLogIn = (e) => {
+        e.preventDefault()
+        const form = e.target
+        const email = form.email.value
+        const password = form.password.value
+        logInUser(email, password)
+        .then(result=>{
+            console.log(result.user)
+            navigate('/')
+        }).catch(err=>{
+            // console.log(err.message)
+            if(err.code == 'auth/invalid-credential'){
+                console.log('auth/invalid-credential')
+            }
+        })
+    }
+
     return (
         <div className="flex justify-center w-full h-[800px] items-center relative">
             <div className="w-[50%]">
@@ -13,17 +34,17 @@ const Login = () => {
                         <h1 className="text-2xl font-bold uppercase">Login</h1>
                         <p>Please login to access your account.</p>
                     </div>
-                    <form className="space-y-4 my-8">
+                    <form className="space-y-4 my-8" onSubmit={handleLogIn}>
                         <div className="space-y-3">
                             <label>Email</label>
                             <div>
-                                <input name="email" placeholder="type email"></input>
+                                <input type="email" required name="email" placeholder="type email"></input>
                             </div>
                         </div>
                         <div className="space-y-3">
                             <label>Password</label>
                             <div>
-                                <input name="password" placeholder="type password"></input>
+                                <input type="password" required name="password" placeholder="type password"></input>
                             </div>
                         </div>
                         <div>
@@ -35,7 +56,7 @@ const Login = () => {
             </div>
             <div className="lg:hidden block absolute top-12 left-10">
                 <FaArrowLeft size={15} />
-            </div>
+            </div> 
 
         </div>
     );
